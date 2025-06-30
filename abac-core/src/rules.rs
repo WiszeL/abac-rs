a84c7aab4d13fb3ea24138af2c9868a7f738ec23
WiszeL <handidwic1225@gmail.com>
2025-06-30 14:45:17 +0700

    Cloneable rules

--- abac-core/src/rules.rs
+++ abac-core/src/rules.rs
@@ -1,14 +1,14 @@
 use serde::Deserialize;
 use serde_value::Value;
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize)]
 pub(crate) enum SideRule {
     Subject(/* Field Name */ String),
     Object(/* Field Name */ String),
     Literal(/* Literal Value */ Value),
 }
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize)]
 pub(crate) enum Operator {
     Equal,
     Greater,
@@ -17,12 +17,12 @@ pub(crate) enum Operator {
     LessEqual,
 }
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize)]
 pub(crate) struct Rule {
     pub(crate) left_rule: SideRule,
     pub(crate) operator: Operator,
     pub(crate) right_rule: SideRule,
 }
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize)]
 pub struct Rules(pub(crate) Vec<Vec<Rule>>);
